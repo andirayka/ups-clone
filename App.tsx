f976e87a94727bce6980799e3019a59d0205447c
andi.rayka <andi.rayka@gmail.com>
2023-02-05 18:51:54 +0700

    add apollo library

--- App.tsx
+++ App.tsx
@@ -4,16 +4,24 @@ import {SafeAreaProvider} from 'react-native-safe-area-context'
 import {TailwindProvider} from 'tailwind-rn'
 import RootNavigator from './src/navigator/RootNavigator'
 import utilities from './tailwind.json'
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/flippant-gnat',
+  cache: new InMemoryCache(),
+})
 
 function App(): JSX.Element {
   return (
     // @ts-ignore
     <TailwindProvider utilities={utilities}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <ApolloProvider client={client}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <RootNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ApolloProvider>
     </TailwindProvider>
   )
 }
