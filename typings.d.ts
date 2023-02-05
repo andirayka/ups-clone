deefabc7b28cef5525cc3ee987f247d3580bd762
andi.rayka <andi.rayka@gmail.com>
2023-02-05 21:33:19 +0700

    add typings

--- typings.d.ts
+++ typings.d.ts
@@ -9,3 +9,37 @@ type CustomerList = {
   name: ID
   value: Customer
 }
+
+type TrackingItem = {
+  customerid: ID
+  customer: Customer
+  items: Item[]
+}
+
+type Item = {
+  item_id: ID
+  name: string
+  price: number
+  quantity: number
+}
+
+type OrderResponse = {
+  value: Order
+}
+
+type CustomerResponse = {
+  name: ID
+  value: Customer
+}
+
+type Order = {
+  carrier: string
+  createdAt: string
+  shippingCost: number
+  trackingId: string
+  trackingItems: TrackingItem
+  Lat: number
+  Lng: number
+  Address: string
+  City: string
+}
